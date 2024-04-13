9e42aecb02fa5206656b93108cfe39bb39a16f4e
gusz0 <166921337+gusz0@users.noreply.github.com>
2024-04-13 14:12:50 +0000

    Aula 0

--- variaveis/Exercicio1.java
+++ variaveis/Exercicio1.java
@@ -1,13 +1,20 @@
 /*
-1. Crie uma variável que informa a primeira letra do seu nome;
-2. Crie uma variável que informa a sua idade;
-3. Crie uma variável informando se você tomou café da manhã;
+1. Crie uma variável que armazena a primeira letra do seu nome;
+2. Crie uma variável que armazena a sua idade;
+3. Crie uma váriavel que armazena sua altura (em metros);
+4. Crie uma variável informando se você tomou café da manhã;
 */
 
 public class Exercicio1 {
     public static void main(String[] args) {
         // Escreva seu código aqui ⬇️
-
-
+        // Exercício 1
+         char letranome = 'G';
+        // Exercício 2
+        int idade = 15;
+        // Exercício 3
+        float alturametros = 1.70;
+        // Exercício 4
+        boolean tomouCafe = true
     }
 }
--- variaveis/Exercicio2.java
+++ variaveis/Exercicio2.java
@@ -1,13 +1,30 @@
 /*
-1. Crie uma variável que armazena o valor de uma laranja;
-2. Crie uma variável que informa o valor de uma uva;
-3. Some o valor da laranja com a uva;
+1. Crie uma variável que armazena o preço de uma laranja;
+2. Crie uma variável que informa o preço de uma uva;
+3. Some o preço da laranja com a uva;
 */
+import java.util.Scanner;
+
 
 public class Exercicio2 {
     public static void main(String[] args) {
-        // Escreva seu código aqui ⬇️
+    
+    
+        Scanner sc = new Scanner(System.in);
 
-        
-    }
+
+        // Escreva seu código aqui ⬇️
+double precolaranja = sc.nextDouble();
+double precomaça = sc.nextDouble();
+double somapreco = precolaranja + precomaça;
+
+System.out.println(somapreco); 
+
+// Recebam duas idades, façam a soma delas e imprima no terminal
+int idade = sc.nextInt();
+int idadeb = sc.nextInt();
+int somaidade = idade + idadeb;
+System.out.println(somaidade);
+
+}
 }
